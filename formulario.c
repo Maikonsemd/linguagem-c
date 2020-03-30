1048c691fa871b981a72158b1d86f3075469ab62
Maikonsemd <48664020+Maikonsemd@users.noreply.github.com>
2020-03-30 01:01:52 -0300

    alteração

--- formulario.c
+++ formulario.c
@@ -1,11 +1,33 @@
 #include<stdio.h>
+#include<locale.h>
 int main(){
+int idade,numero,telefone,opc,subopc,tippaln;
+char nome[100],end[100],rua[100],bairro[100],email[100];
 
-int a,b,c;
-char nome;
+printf("digite por favor seu nome:\n");
+scanf("%s",&nome);
+printf("Seja bem vindo %s",nome);
 
-printf("digite seu nome");
-scanf("%c"&nome);
+printf("Como podemos lhe ajudar:\n Digite 1 - para compra de planos \n\n Digite 2- para mais servicos\n\n");
+scanf("%d",&opc);
+switch (opc)
+{
+case 1:
+printf(" %d digite por favor sua idade :\n",nome);
+scanf("%d",&idade);
+printf("digite por favor seu email:\n");
+scanf("%d",&email);
 
-printf("seu nome é %c",nome);
+break;
+case 2:
+printf("%s digite 1- para dúvidas sobre seu palno \n\n digite 2- para mudança de plano ");
+scanf("%d",&subopc);
+printf("qual o seu palno:\n\n");
+
+break;
+
+default:
+printf("Opção invalida, agardecemos a sua atenção!");
+    break;
+}
 }
