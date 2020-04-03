3b9cb72d7e33181d3781e3c08eddcce0c779f651
Maikonsemd <48664020+Maikonsemd@users.noreply.github.com>
2020-04-03 10:47:23 -0300

    alteração

--- formulario.c
+++ formulario.c
@@ -8,19 +8,19 @@ printf("digite por favor seu nome:\n");
 scanf("%s",&nome);
 printf("Seja bem vindo %s",nome);
 
-printf("Como podemos lhe ajudar:\n Digite 1 - para compra de planos \n\n Digite 2- para mais servicos\n\n");
+printf("\n Como podemos lhe ajudar:\n Digite 1 - Para comprar nossos planos: \n\n Digite 2- Para mais servicos \n\n");
 scanf("%d",&opc);
 switch (opc)
 {
 case 1:
-printf(" %d digite por favor sua idade :\n",nome);
+printf("Digite por favor sua idade :\n",nome);
 scanf("%d",&idade);
-printf("digite por favor seu email:\n");
+printf("Digite por favor seu email:\n");
 scanf("%d",&email);
 
 break;
 case 2:
-printf("%s digite 1- para dúvidas sobre seu plano\n\n digite 2- para mudança de plano ");
+printf("Digite 1 - para duvidas sobre seu plano\n\n digite 2 - para mudanca de plano");
 scanf("%d",&subopc);
 printf("qual o seu palno:\n\n");
 
