926d7bac2da31ada81b64b2bc85de9bae5d8eca6
Maikonsemd <48664020+Maikonsemd@users.noreply.github.com>
2020-04-04 16:00:37 -0300

    alterações

---
+++ arquivos/aula2.c
@@ -0,0 +1,29 @@
+// Escrevendo um caractere em um arquivo
+// Escreva um programa que peça um caractere para o usuário
+//  e salve esta entrada em um arquivo chamado "char.txt", 
+//  localizado na mesma pasta do programa executável.
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    char url[]= "dado.txt";
+    FILE *arq;
+    char letra;
+
+    printf("Caractere:");
+    letra=getchar();
+
+    arq=fopen(url,"w");
+    if(arq== NULL){
+    printf("erro");
+    }
+    else{
+    fputc(letra, arq);
+    fclose(arq);          
+    }
+    
+    return 0;
+
+
+}
---
+++ arquivos/formulario.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    char url[]="notas.txt";
+    float nota,
+    media=0.0;
+    FILE *arq;
+
+    arq=fopen(url, "w");
+    if(arq == NULL)
+    printf("Erro, não foi possivel abrir o arquivo\n");
+    else{
+    printf("Nota de Matematica:\n");
+    scanf("%f", & nota);
+    fprintf(arq, "Matematica: %.2f\n", nota);
+    media+=nota;
+    printf("Nota de Fisica:\n");
+    scanf("%f", & nota);
+    fprintf(arq, "Fisica: %.2f\n", nota);
+    media+=nota;
+     printf("Nota de Quimica:\n");
+    scanf("%f", & nota);
+    fprintf(arq, "Quimica: %.2f\n", nota);
+    media+=nota;
+
+    media/=3;
+    fprintf(arq, "Media final: %2.f\n",media);
+    }
+    fclose(arq);
+    printf("Dados gravados com sucesso!");
+    return 0;
+    
+   
+    
+}
+   
