7252704e3fc225c54da84329f86ce94748bce1c8
Maikonsemd <48664020+Maikonsemd@users.noreply.github.com>
2020-04-19 15:21:58 -0300

    alterações

---
+++ Vetores/Aula1.c
@@ -0,0 +1,23 @@
+//declarando um vetor com várias notas
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+
+int main(){
+
+    float  notas[5]={1,2,3,4,5,6};
+
+  printf("Exibindo os Valores do Vetor \n\n");
+  printf("notas[0] = %.1f\n", notas[0]);
+  printf("notas[1] = %.1f\n", notas[1]);
+  printf("notas[2] = %.1f\n", notas[2]);
+  printf("notas[3] = %.1f\n", notas[3]);
+  printf("notas[4] = %.1f\n", notas[4]);
+  printf("notas[4] = %.1f\n", notas[4]);
+
+
+  return 0;
+
+}
